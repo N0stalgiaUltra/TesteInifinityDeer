c7a549b3d60c461e8c0cc5cd8849b3fc5d215ae2
Vinicius Santos <viniciusantos0898@gmail.com>
2022-06-29 14:01:51 -0300

    Adicionado Navmesh para inimigo; Adicionada perseguição ao player por parte do zumbi

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private GameObject playerTest;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        agent.SetDestination(playerTest.transform.position);
+        transform.LookAt(playerTest.transform);
+
+        //agent.SetDestination(GameManager.instance.ReturnNearestPlayer(this.transform).transform.position);
+    }
+
+    private void Attack()
+    {
+        agent.SetDestination(this.transform.position);
+        transform.LookAt(playerTest.transform);
+
     }
 }
